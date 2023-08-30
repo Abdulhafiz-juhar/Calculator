7bd2a0c286904c75b91de5182b123f4ea2a73976
Abdulhafiz-juhar <abdulhafizjuharajmbest@gmail.com>
2023-08-30 15:02:24 +0300

    Basic UI improvement

--- script.js
+++ script.js
@@ -1,219 +1,250 @@
 let firstNum, operator, secondNum;
 
-function add(a,b) {
-    return a + b;
+function add(a, b) {
+  return a + b;
 }
 
-function substract(a,b) {
-    return a - b;
+function substract(a, b) {
+  return a - b;
 }
 
-function multiply(a,b) {
-    return a * b;
+function multiply(a, b) {
+  return a * b;
 }
 
-function divide(a,b) {
-    return a / b;
+function divide(a, b) {
+  return a / b;
 }
 
-function operate(fNum=0,op,sNum=0) {
-    return  (op === '+') ? add(fNum,sNum) :
-            (op === '-') ? substract(fNum,sNum) :
-            (op === '*') ? multiply(fNum,sNum) :
-            (op === '/') ? divide(fNum,sNum) :
-            null;
+function operate(fNum = 0, op, sNum = 0) {
+  return op === "+"
+    ? add(fNum, sNum)
+    : op === "-"
+    ? substract(fNum, sNum)
+    : op === "*"
+    ? multiply(fNum, sNum)
+    : op === "/"
+    ? divide(fNum, sNum)
+    : null;
 }
 
-const display = document.querySelector('.display');
-const dot = document.querySelector('.dot');
+const display = document.querySelector(".display");
+const dot = document.querySelector(".dot");
 
 let clicked = [];
 let result = 0;
 let clearResult = false;
 
 function reset() {
-    clicked = [];
-    result = 0;
-    display.textContent = '';
+  clicked = [];
+  result = 0;
+  display.textContent = "";
 }
 
 // turn off dot button when a dot per one element exists or if element
 // is operator
 function turnoffDot(element) {
-    if(!isNaN(parseFloat(element))){
-        let stringElem = element.toString();
-        return stringElem.split('').some(char => char === '.');
-    } else {
-        return true;
-    } 
+  if (!isNaN(parseFloat(element))) {
+    let stringElem = element.toString();
+    return stringElem.split("").some((char) => char === ".");
+  } else {
+    return true;
+  }
 }
 
-function handleTurnoffButton(statusFunc,array,element,executeFunc) {
-    if(!statusFunc(array[array.length-1])) {
-        if(!element.hasAttribute(`${executeFunc.name}`)) {
-            element.addEventListener('click', executeFunc);
-            element.setAttribute(`${executeFunc.name}`,'true');
-        }
-    } else {
-        if(element.hasAttribute(`${executeFunc.name}`)) {
-            element.removeEventListener('click',executeFunc);
-            element.removeAttribute(`${executeFunc.name}`);
-        }
+function handleTurnoffButton(statusFunc, array, element, executeFunc) {
+  if (!statusFunc(array[array.length - 1])) {
+    if (!element.hasAttribute(`${executeFunc.name}`)) {
+      element.addEventListener("click", executeFunc);
+      element.setAttribute(`${executeFunc.name}`, "true");
     }
+  } else {
+    if (element.hasAttribute(`${executeFunc.name}`)) {
+      element.removeEventListener("click", executeFunc);
+      element.removeAttribute(`${executeFunc.name}`);
+    }
+  }
 }
 
 function displayResult(element, array) {
-    if(!isNaN(array[0])) {
-        for (current of array) {
-            element.textContent += `${current} `;
-        }
-    } else {
-        array.pop();
+  if (!isNaN(array[0])) {
+    for (current of array) {
+      element.textContent += `${current} `;
     }
+  } else {
+    array.pop();
+  }
 }
 
-function saveDisplay (e) {
-    let input = e.key || this.textContent
-// check if new calculation is started after result
-    if(clearResult) {
-        if(!isNaN(parseInt(input))) {
-            reset();
-        }
-        clearResult = false;
+function saveDisplay(e) {
+  let input = e.key || this.textContent;
+  // check if new calculation is started after result
+  if (clearResult) {
+    if (!isNaN(parseInt(input))) {
+      reset();
+    }
+    clearResult = false;
+  }
+
+  display.textContent = "";
+  clicked.push(input);
+
+  // 0 before number checker
+  if (clicked.length >= 2) {
+    if (clicked[0] === "0" && !isNaN(parseInt(clicked[1]))) {
+      clicked[0] = clicked[1];
+      clicked.pop();
     }
 
-    display.textContent = '';
-    clicked.push(input);
-
-// 0 before number checker
-    if(clicked.length >=2) {
-        if(clicked[0] === '0' && !isNaN(parseInt(clicked[1]))) {
-            clicked[0] = clicked[1];
-            clicked.pop();
-        }
-
-        if(clicked[2] === '0' && !isNaN(parseInt(clicked[3]))) {
-            clicked[2] = clicked[3];
-            clicked.pop();
-        }
-    }    
-
-
-// For concating consequetive numbers in array if multiple digit
-// is entered (check dot too)
-    if(clicked.length >= 2) {
-        if(!isNaN(parseInt(clicked[clicked.length-1])) === !isNaN(parseInt(clicked[clicked.length-2])) || (!isNaN(parseInt(clicked[clicked.length-2])) && clicked[clicked.length-1] === '.')) {
-            if(!isNaN(parseInt(clicked[clicked.length-1])) || clicked[clicked.length-1] === '.') {
-                let combiner = clicked.slice(clicked.length-2).join('');
-                clicked.splice(clicked.length-2,2);
-                clicked.push(combiner);
-
-                console.log('combiner');
-                console.log(combiner);
-                console.log('combiner');
-            } else {
-                clicked[clicked.length-2] = clicked[clicked.length-1];
-                clicked.pop();
-            }
-        }
+    if (clicked[2] === "0" && !isNaN(parseInt(clicked[3]))) {
+      clicked[2] = clicked[3];
+      clicked.pop();
     }
+  }
 
-//check if '=' is entered just after one number
-    if(clicked.length >= 2 && clicked[1] === '=') {
+  // For concating consequetive numbers in array if multiple digit
+  // is entered (check dot too)
+  if (clicked.length >= 2) {
+    if (
+      !isNaN(parseInt(clicked[clicked.length - 1])) ===
+        !isNaN(parseInt(clicked[clicked.length - 2])) ||
+      (!isNaN(parseInt(clicked[clicked.length - 2])) &&
+        clicked[clicked.length - 1] === ".")
+    ) {
+      if (
+        !isNaN(parseInt(clicked[clicked.length - 1])) ||
+        clicked[clicked.length - 1] === "."
+      ) {
+        let combiner = clicked.slice(clicked.length - 2).join("");
+        clicked.splice(clicked.length - 2, 2);
+        clicked.push(combiner);
+
+        console.log("combiner");
+        console.log(combiner);
+        console.log("combiner");
+      } else {
+        clicked[clicked.length - 2] = clicked[clicked.length - 1];
         clicked.pop();
+      }
+    }
+  }
+
+  //check if '=' is entered just after one number
+  if (clicked.length >= 2 && clicked[1] === "=") {
+    clicked.pop();
+  }
+
+  // perform  math operation once the required elements are available
+  if (
+    clicked.length >= 4 ||
+    (clicked.length >= 3 && clicked[clicked.length - 1] === "=")
+  ) {
+    result = operate(Number(clicked[0]), clicked[1], Number(clicked[2]));
+    //check if result has more than one decimal
+    if (Math.round(result * 100) % 100 === 0) {
+      result = Math.round(result);
+    } else {
+      result = Math.round(result * 100) / 100;
     }
 
-// perform  math operation once the required elements are available
-        if(clicked.length >= 4 || (clicked.length >= 3 && clicked[clicked.length-1] === '=')) {
-                result = operate(Number(clicked[0]),clicked[1],Number(clicked[2]));
-            //check if result has more than one decimal
-            if (Math.round(result * 100) % 100 === 0) {
-                result = Math.round(result);
-              } else {
-                result = Math.round(result * 100) / 100;
-              }
+    result = result.toString();
+    clicked[0] = result;
+    if (clicked[clicked.length - 1] === "=") {
+      clicked.splice(1, 3);
+      // track to clear result when new number is entered after result
+      clearResult = true;
+    } else {
+      clicked.splice(1, 2);
+    }
+  }
 
-            result = result.toString();
-            clicked[0] = result;
-            if(clicked[clicked.length-1]=== '=') {
-                clicked.splice(1,3);
-                // track to clear result when new number is entered after result
-                clearResult = true;
-            } else {
-                clicked.splice(1,2);
-            }
-        }
+  displayResult(display, clicked);
 
-        
-    displayResult(display,clicked);
+  // toggle dot button
+  handleTurnoffButton(turnoffDot, clicked, dot, saveDisplay);
 
-// toggle dot button
-    handleTurnoffButton(turnoffDot,clicked,dot,saveDisplay);
-        
-          
-    console.log(clicked);
+  console.log(clicked);
 }
 
 function handleBackspace(arr) {
-    let element = arr[arr.length-1];
-    console.log(element);
-    if(isNaN(element)){
-        arr.pop();
-        display.textContent = '';
-        displayResult(display,clicked);
-        handleTurnoffButton(turnoffDot,clicked,dot,saveDisplay);
+  let element = arr[arr.length - 1];
+  console.log(element);
+  if (isNaN(element)) {
+    arr.pop();
+    display.textContent = "";
+    displayResult(display, clicked);
+    handleTurnoffButton(turnoffDot, clicked, dot, saveDisplay);
+  } else {
+    let newElement;
+    let splittedElement = element.split("");
+    if (splittedElement.length === 1) {
+      arr.pop();
+      display.textContent = "";
+      displayResult(display, clicked);
+      handleTurnoffButton(turnoffDot, clicked, dot, saveDisplay);
     } else {
-        let newElement;
-        let splittedElement = element.split('');
-        if(splittedElement.length === 1) {
-            arr.pop();
-            display.textContent = '';
-            displayResult(display,clicked);
-            handleTurnoffButton(turnoffDot,clicked,dot,saveDisplay);
-        } else {
-            splittedElement.pop();
-            newElement = splittedElement.join('');
-            arr.pop();
-            arr.push(newElement);
-            display.textContent = '';
-            displayResult(display,clicked);
-            handleTurnoffButton(turnoffDot,clicked,dot,saveDisplay);
-        }
+      splittedElement.pop();
+      newElement = splittedElement.join("");
+      arr.pop();
+      arr.push(newElement);
+      display.textContent = "";
+      displayResult(display, clicked);
+      handleTurnoffButton(turnoffDot, clicked, dot, saveDisplay);
     }
+  }
 }
 
-const buttons = document.querySelectorAll('.buttonOutput');
-buttons.forEach(button => {
-    button.addEventListener('click', saveDisplay)
-})
+const buttons = document.querySelectorAll(".buttonOutput");
+buttons.forEach((button) => {
+  button.addEventListener("click", saveDisplay);
+});
 
-const clear = document.querySelector('.clear');
-clear.addEventListener('click', reset);
+const clear = document.querySelector(".clear");
+clear.addEventListener("click", reset);
 
-const backspace = document.querySelector('.backspace');
-backspace.addEventListener('click',() => handleBackspace(clicked));
-
+const backspace = document.querySelector(".backspace");
+backspace.addEventListener("click", () => handleBackspace(clicked));
 
 //keyboard
-let allowedKeys = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '=', '+', '.', '-', '*', '/', 'Backspace','Enter','Delete'];
+let allowedKeys = [
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "=",
+  "+",
+  ".",
+  "-",
+  "*",
+  "/",
+  "Backspace",
+  "Enter",
+  "Delete",
+];
 
-window.addEventListener('keydown', function(e) {
-    if(allowedKeys.includes(e.key)) {
-        if(e.key === 'Backspace') {
-            handleBackspace(clicked);
-        } else if(e.key === 'Delete') {
-            reset();
-        } else if(e.key === 'Enter') {
-            e.preventDefault();
-            var newEvent = new KeyboardEvent('keydown', { key: '=' });
-            saveDisplay(newEvent);
-        } else {
-            saveDisplay(e);
-        }
+window.addEventListener("keydown", function (e) {
+  if (allowedKeys.includes(e.key)) {
+    if (e.key === "Backspace") {
+      handleBackspace(clicked);
+    } else if (e.key === "Delete") {
+      reset();
+    } else if (e.key === "Enter") {
+      e.preventDefault();
+      var newEvent = new KeyboardEvent("keydown", { key: "=" });
+      saveDisplay(newEvent);
+    } else {
+      saveDisplay(e);
     }
+  }
 });
 //issue
 //dot doesn't clear the result as the numbers(imporved)
-    //now it addes the dot to the result
+//now it addes the dot to the result
 //can't input negative number first
 //divide by 0 gives infinity instead of snarky message
